6933d1e46f4eb84466b4be52f89fa6999ef171d9
Beulah16 <igboanugobeulah16@gmail.com>
2025-04-15 17:54:38 +0100

    Register api endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { config } from "dotenv";
 import express from "express";
 import { AppDataSource } from "./data-source";
+import apiEndPoints from "./routes"
 
 config();
 
 const app = express();
+
+app.use(express.json())
+
+app.use('/api', apiEndPoints)
 
 const PORT = process.env.PORT;
 
@@ -12,6 +17,6 @@ AppDataSource.initialize()
     .then(() => {
         console.log("Data Source has been initialized!");
 
-        app.listen(PORT, () => console.log(`App is listening on ${PORT}`))
+        app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
     })
     .catch((error) => console.error("Error during Data Source initialization:", error));
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export enum MediaType {
+    VIDEO = 'video',
+    AUDIO = 'audio',
+    PDF = 'pdf'
+}
+export enum MinistartionType {
+    MESSAGE = 'message',
+    SONG = 'song',
+    TESTIMONY = 'testimony',
+    SHOW = 'show'
+}
