9e5d40e459d2d240f114e9bc095b3e4ad86ae5c9
Beulah16 <igboanugobeulah16@gmail.com>
2025-04-18 17:03:33 +0100

    create endpoints for CRUD operations on Years

--- src/controllers/YearController.ts
+++ src/controllers/YearController.ts
@@ -1,9 +1,66 @@
 import { Request, Response } from "express";
 import { Year } from "../models/Year";
 
+export const getAll = async (req: Request, res: Response) => {
+
+    const years = await Year.find()
+    res.json(years);
+};
+
 export const create = async (req: Request, res: Response) => {
 
     const response = await Year.save(req.body)
 
     res.json(response);
 }
+
+export const getOne = async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+
+    const year = await Year.findOneBy({ id });
+    if (!year) {
+        res.json({ message: "Year not found" })
+        return
+    }
+
+    res.json(year);
+};
+
+export const update = async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+    const {name} = req.body;
+
+    try {
+        const year = await Year.findOneBy({ id });
+        if (!year) {
+            res.json({ message: "Year not found" })
+            return
+        }
+        year.name = name;
+        await year.save();
+        res.json({ message: "Year updated successfully" });
+    } 
+    catch (err: any) {
+        console.log({error: err.message})
+        res.json({ message: err.message });
+    }
+
+};
+
+
+export const remove = async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+
+    const year = await Year.findOneBy({ id });
+    if (year)
+    {
+        await year.softRemove();
+        res.json({ message: "Year deleted successfully" });
+    }
+    else {
+        res.json({ message: "Year does not exist" });
+    }
+};
--- src/routes/years.ts
+++ src/routes/years.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { create } from "../controllers/YearController";
+import { create, getAll, getOne, remove, update } from "../controllers/YearController";
 
 const router = Router();
 
+router.get("/", getAll);
 router.post("/", create);
+router.get("/:id", getOne);
+router.put("/:id", update);
+router.delete("/:id", remove);
 
 
 export default router;
